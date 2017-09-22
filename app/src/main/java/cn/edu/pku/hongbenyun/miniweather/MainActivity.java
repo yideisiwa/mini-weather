59d17f94aa421424398f96362c3f935259d79ab9
hongbenyun <396138749@qq.com>
2017-09-22 08:45:21 +0800

    fitst commit

---
+++ app/src/main/java/cn/edu/pku/hongbenyun/miniweather/MainActivity.java
@@ -0,0 +1,13 @@
+package cn.edu.pku.hongbenyun.miniweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
